a22cf9cfa923deb1d78dc90f1ac810bc770f84e2
mkoloni <vmwangi550@gmail.com>
2020-10-28 14:42:16 +0300

    On branch master
     Initial commit
    
     Changes to be committed:
    	new file:   README.md
    	new file:   aliases/git.fish
    	new file:   aliases/main.fish
    	new file:   config.fish
    	new file:   env/go.fish
    	new file:   env/index.fish
    	new file:   env/mysql_5_7.fish
    	new file:   env/nodenv.fish
    	new file:   env/pyenv.fish
    	new file:   env/rbenv.fish
    	new file:   fish_variables

---
+++ aliases/git.fish
@@ -0,0 +1,8 @@
+alias gl="git log"
+alias ga="git add ."
+alias gc="git commit ."
+alias gb="git branch"
+alias gs="git status"
+alias gpull="git pull"
+alias gpush="git push"
+alias gsw="git switch"
---
+++ aliases/main.fish
@@ -0,0 +1,14 @@
+alias ve='cd; nvim .vimrc'
+alias vr='nvim .zshrc'
+alias vi="nvim"
+alias cl='clear'
+alias u='uptime'
+alias off='shutdown -h now'
+alias rm="rm -rf"
+alias cp="cp -rf"
+alias py3="python3"
+alias rmd="recordmydesktop"
+alias exit="exit; exit"
+# controll screen brightness
+alias brightness="xrandr --output LVDS --brightness"
+
